e6756c671bff42bf3dbc56e67a73439d2e631a27
Xinrui Zu <xrzu14@fudan.edu.cn>
2025-04-19 12:39:36 +0200

    home

--- notebooks/env.py
+++ notebooks/env.py
@@ -18,6 +18,8 @@ from brax.envs.base import PipelineEnv, State
 from brax.io import mjcf
 from etils import epath
 
+from typing import Tuple
+
 import wandb
 import xmltodict
 
@@ -88,6 +90,9 @@ def adapt_xml(file, design=None):
     return bth_r, bsh_r, bfo_r, fth_r, fsh_r, ffo_r
 
 
+"""
+mujoco half_cheetah in brax:
+"""
 class HalfcheetahWithObstacles(PipelineEnv):
 
     def __init__(
@@ -232,3 +237,123 @@ class HalfcheetahWithObstacles(PipelineEnv):
             position = position[1:]
 
         return jp.concatenate((position, velocity))
+
+
+"""
+mujoco hopper in brax:
+"""
+class Hopper(PipelineEnv):
+
+  def __init__(
+      self,
+      forward_reward_weight: float = 1.0,
+      ctrl_cost_weight: float = 1e-3,
+      healthy_reward: float = 1.0,
+      terminate_when_unhealthy: bool = True,
+      healthy_state_range=(-100.0, 100.0),
+      healthy_z_range: Tuple[float, float] = (0.7, float('inf')),
+      healthy_angle_range=(-0.2, 0.2),
+      reset_noise_scale=5e-3,
+      exclude_current_positions_from_observation=True,
+      backend='generalized',
+      **kwargs
+  ):
+    path = epath.resource_path('brax') / 'envs/assets/hopper.xml'
+    sys = mjcf.load(path)
+
+    n_frames = 4
+    kwargs['n_frames'] = kwargs.get('n_frames', n_frames)
+
+    super().__init__(sys=sys, backend=backend, **kwargs)
+
+    self._forward_reward_weight = forward_reward_weight
+    self._ctrl_cost_weight = ctrl_cost_weight
+    self._healthy_reward = healthy_reward
+    self._terminate_when_unhealthy = terminate_when_unhealthy
+    self._healthy_state_range = healthy_state_range
+    self._healthy_z_range = healthy_z_range
+    self._healthy_angle_range = healthy_angle_range
+    self._reset_noise_scale = reset_noise_scale
+    self._exclude_current_positions_from_observation = (
+        exclude_current_positions_from_observation
+    )
+
+  def reset(self, rng: jax.Array) -> State:
+    """Resets the environment to an initial state."""
+    rng, rng1, rng2 = jax.random.split(rng, 3)
+
+    low, hi = -self._reset_noise_scale, self._reset_noise_scale
+    qpos = self.sys.init_q + jax.random.uniform(
+        rng1, (self.sys.q_size(),), minval=low, maxval=hi
+    )
+    qvel = jax.random.uniform(
+        rng2, (self.sys.qd_size(),), minval=low, maxval=hi
+    )
+
+    pipeline_state = self.pipeline_init(qpos, qvel)
+
+    obs = self._get_obs(pipeline_state)
+    reward, done, zero = jp.zeros(3)
+    metrics = {
+        'reward_forward': zero,
+        'reward_ctrl': zero,
+        'reward_healthy': zero,
+        'x_position': zero,
+        'x_velocity': zero,
+    }
+    return State(pipeline_state, obs, reward, done, metrics)
+
+  def step(self, state: State, action: jax.Array) -> State:
+    """Runs one timestep of the environment's dynamics."""
+    pipeline_state0 = state.pipeline_state
+    assert pipeline_state0 is not None
+    pipeline_state = self.pipeline_step(pipeline_state0, action)
+
+    x_velocity = (
+        pipeline_state.x.pos[0, 0] - pipeline_state0.x.pos[0, 0]
+    ) / self.dt
+    forward_reward = self._forward_reward_weight * x_velocity
+
+    z, angle = pipeline_state.x.pos[0, 2], pipeline_state.q[2]
+    state_vec = jp.concatenate([pipeline_state.q[2:], pipeline_state.qd])
+    min_z, max_z = self._healthy_z_range
+    min_angle, max_angle = self._healthy_angle_range
+    min_state, max_state = self._healthy_state_range
+    is_healthy = jp.all(
+        jp.logical_and(min_state < state_vec, state_vec < max_state)
+    )
+    is_healthy &= jp.logical_and(min_z < z, z < max_z)
+    is_healthy &= jp.logical_and(min_angle < angle, angle < max_angle)
+    if self._terminate_when_unhealthy:
+      healthy_reward = self._healthy_reward
+    else:
+      healthy_reward = self._healthy_reward * is_healthy
+
+    ctrl_cost = self._ctrl_cost_weight * jp.sum(jp.square(action))
+
+    obs = self._get_obs(pipeline_state)
+    reward = forward_reward + healthy_reward - ctrl_cost
+    done = 1.0 - is_healthy if self._terminate_when_unhealthy else 0.0
+    state.metrics.update(
+        reward_forward=forward_reward,
+        reward_ctrl=-ctrl_cost,
+        reward_healthy=healthy_reward,
+        x_position=pipeline_state.x.pos[0, 0],
+        x_velocity=x_velocity,
+    )
+
+    return state.replace(
+        pipeline_state=pipeline_state, obs=obs, reward=reward, done=done
+    )
+
+  def _get_obs(self, pipeline_state: base.State) -> jax.Array:
+    """Returns the environment observations."""
+    position = pipeline_state.q
+    position = position.at[1].set(pipeline_state.x.pos[0, 2])
+    velocity = jp.clip(pipeline_state.qd, -10, 10)
+
+    if self._exclude_current_positions_from_observation:
+      position = position[1:]
+
+    return jp.concatenate((position, velocity))
+
